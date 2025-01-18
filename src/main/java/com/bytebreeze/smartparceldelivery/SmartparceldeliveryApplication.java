2ef3a97dbaa6d669d35c9e2f4e436d83571c4077
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2025-01-18 21:52:26 +0600

    Initial porject setup

---
+++ src/main/java/com/bytebreeze/smartparceldelivery/SmartparceldeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.bytebreeze.smartparceldelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartparceldeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartparceldeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytebreeze/smartparceldelivery/SmartparceldeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytebreeze.smartparceldelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartparceldeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
