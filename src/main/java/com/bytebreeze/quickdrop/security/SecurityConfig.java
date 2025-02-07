fdb6e34ba6770c8a79625b536cbaf3ae191affa8
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2025-02-08 00:07:20 +0600

    Update SecurityConfig with custom userdetailsservice

--- src/main/java/com/bytebreeze/quickdrop/security/SecurityConfig.java
+++ src/main/java/com/bytebreeze/quickdrop/security/SecurityConfig.java
@@ -1,12 +1,21 @@
 package com.bytebreeze.quickdrop.security;
 
+import com.bytebreeze.quickdrop.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return new CustomUserDetailsService(userRepository);
+    }
+    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -26,5 +35,10 @@ public class SecurityConfig {
                         logout.permitAll()
                 );
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
