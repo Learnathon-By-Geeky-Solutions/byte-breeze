4df383119d132e434f316d0b4f5dfb9e6521ac3b
Khairul Basar <khairulbashar24853@gmail.com>
2025-02-11 18:27:52 +0600

    Add DTO for user registration request

---
+++ src/main/java/com/bytebreeze/quickdrop/dto/UserRegistrationRequestDTO.java
@@ -0,0 +1,26 @@
+package com.bytebreeze.quickdrop.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRegistrationRequestDTO {
+
+    @NotBlank(message = "Name can not be empty")
+    private String fullName;
+
+    @NotBlank(message = "Email can not be empty")
+    @Email(message = "Invalid Email format")
+    private String email;
+
+    @NotBlank(message = "Password can not be empty")
+    @Size(min = 6, message = "Password must be at least 6 characters long" )
+    private String password;
+
+
+}
