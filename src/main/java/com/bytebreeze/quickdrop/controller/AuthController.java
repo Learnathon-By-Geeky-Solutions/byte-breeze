041cd42697adbeeae35b42fc84d4d34267ceaa2d
Khairul Basar <khairulbashar24853@gmail.com>
2025-02-13 15:38:14 +0600

    Resolve some issues

--- src/main/java/com/bytebreeze/quickdrop/controller/AuthController.java
+++ src/main/java/com/bytebreeze/quickdrop/controller/AuthController.java
@@ -4,7 +4,6 @@ import com.bytebreeze.quickdrop.dto.UserRegistrationRequestDTO;
 import com.bytebreeze.quickdrop.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
--- src/main/java/com/bytebreeze/quickdrop/dto/UserRegistrationRequestDTO.java
+++ src/main/java/com/bytebreeze/quickdrop/dto/UserRegistrationRequestDTO.java
@@ -1,11 +1,11 @@
 package com.bytebreeze.quickdrop.dto;
 
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 
 @Getter
