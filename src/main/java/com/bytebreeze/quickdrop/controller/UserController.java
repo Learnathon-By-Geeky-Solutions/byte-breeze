7d8e79b625cd483c8054d9ead6abf5f2c16f07b4
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2025-02-17 01:10:46 +0600

    Update UserController.java to implement profile update route

--- src/main/java/com/bytebreeze/quickdrop/controller/UserController.java
+++ src/main/java/com/bytebreeze/quickdrop/controller/UserController.java
@@ -1,14 +1,37 @@
 package com.bytebreeze.quickdrop.controller;
 
+import com.bytebreeze.quickdrop.dto.UserProfileUpdateDto;
+import com.bytebreeze.quickdrop.model.User;
+import com.bytebreeze.quickdrop.repository.UserRepository;
+import com.bytebreeze.quickdrop.service.UserService;
+import com.bytebreeze.quickdrop.util.AuthUtil;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
-@RequestMapping
+@RequestMapping("/user")
 public class UserController {
-    @GetMapping("/user/dashboard")
+    private final UserRepository userRepository;
+    private final UserService userService;
+
+    public UserController(UserRepository userRepository, UserService userService) {
+        this.userRepository = userRepository;
+        this.userService = userService;
+    }
+
+    @GetMapping("/dashboard")
     public String userDashboard() {
         return "dashboard/home";
+    }
+
+    @GetMapping("/profile-settings")
+    public String basicProfileSettings(Model model)
+    {
+        model.addAttribute("userProfileUpdateDto", userService.userProfileUpdateGet());
+        return "dashboard/account";
     }
 }
