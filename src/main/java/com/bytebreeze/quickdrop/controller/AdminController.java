d9a467357ce738657457f895275a2597d66cc683
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2025-02-17 00:20:58 +0600

    Update AdminController.java to correct url mapping

--- src/main/java/com/bytebreeze/quickdrop/controller/AdminController.java
+++ src/main/java/com/bytebreeze/quickdrop/controller/AdminController.java
@@ -2,16 +2,18 @@ package com.bytebreeze.quickdrop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
 
-    @GetMapping("/admin/login")
+    @GetMapping("/login")
     public String adminLogin() {
         return "auth/admin-login"; // Renders `admin-login.html`
     }
 
-    @GetMapping("/admin/dashboard")
+    @GetMapping("/dashboard")
     public String adminDashboard() {
         return "dashboard/admin-dashboard"; // Renders `admin-dashboard.html`
     }
