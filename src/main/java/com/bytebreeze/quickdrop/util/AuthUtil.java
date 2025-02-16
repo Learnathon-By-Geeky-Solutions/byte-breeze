1de1bc7dc94b3de4146d1febee0145cdde376134
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2025-02-17 01:07:47 +0600

    Add AuthUtil.java file

---
+++ src/main/java/com/bytebreeze/quickdrop/util/AuthUtil.java
@@ -0,0 +1,16 @@
+package com.bytebreeze.quickdrop.util;
+
+import com.bytebreeze.quickdrop.security.CustomUserDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class AuthUtil {
+    public static String getAuthenticatedUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+            return ((UserDetails) authentication.getPrincipal()).getUsername();
+        }
+        return authentication != null ? authentication.getPrincipal().toString() : null;
+    }
+}
