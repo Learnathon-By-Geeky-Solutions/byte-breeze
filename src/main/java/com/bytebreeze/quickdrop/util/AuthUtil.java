6631886d1783c98020f41ea2f9a7724b6aac765f
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2025-02-17 01:09:41 +0600

    Update UserService.java file

--- src/main/java/com/bytebreeze/quickdrop/util/AuthUtil.java
+++ src/main/java/com/bytebreeze/quickdrop/util/AuthUtil.java
@@ -1,6 +1,5 @@
 package com.bytebreeze.quickdrop.util;
 
-import com.bytebreeze.quickdrop.security.CustomUserDetails;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
