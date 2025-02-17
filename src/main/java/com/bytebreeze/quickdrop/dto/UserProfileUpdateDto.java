203481f37af0f1b28312599021c57dc0230ce441
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2025-02-17 23:34:17 +0600

    Create UserProfileUpdateDto.java file

--- src/main/java/com/bytebreeze/quickdrop/dto/UserProfileUpdateDto.java
+++ src/main/java/com/bytebreeze/quickdrop/dto/UserProfileUpdateDto.java
@@ -8,8 +8,9 @@ public class UserProfileUpdateDto {
     @NotEmpty(message = "Name must not be empty.")
     private String fullName;
 
-    @NotEmpty(message = "Password must not be empty.")
     private String password;
 
     private String phoneNumber;
+
+    private String profileImageUrl;
 }
