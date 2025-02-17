d8572a3c8d24ef5a8b2c1bffaf61c89f4924d575
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2025-02-17 23:42:35 +0600

    Add user profile update functionality

--- src/main/java/com/bytebreeze/quickdrop/controller/UserController.java
+++ src/main/java/com/bytebreeze/quickdrop/controller/UserController.java
@@ -1,25 +1,22 @@
 package com.bytebreeze.quickdrop.controller;
 
 import com.bytebreeze.quickdrop.dto.UserProfileUpdateDto;
-import com.bytebreeze.quickdrop.model.User;
-import com.bytebreeze.quickdrop.repository.UserRepository;
 import com.bytebreeze.quickdrop.service.UserService;
-import com.bytebreeze.quickdrop.util.AuthUtil;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
-    private final UserRepository userRepository;
     private final UserService userService;
+    private static final String DASHBOARD_PROFILE_SETTINGS_PAGE = "/dashboard/account";
 
-    public UserController(UserRepository userRepository, UserService userService) {
-        this.userRepository = userRepository;
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
@@ -32,6 +29,29 @@ public class UserController {
     public String basicProfileSettings(Model model)
     {
         model.addAttribute("userProfileUpdateDto", userService.userProfileUpdateGet());
-        return "dashboard/account";
+        return DASHBOARD_PROFILE_SETTINGS_PAGE;
+    }
+
+    @PostMapping("/profile-settings")
+    public String updateAccountSettings(
+            @Valid @ModelAttribute("userProfileUpdateDto") UserProfileUpdateDto dto,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes,
+            Model model)
+    {
+
+        if (bindingResult.hasErrors()) {
+            return DASHBOARD_PROFILE_SETTINGS_PAGE;
+        }
+
+        try {
+            userService.updateUserProfile(dto);
+        } catch (Exception e) {
+            model.addAttribute("updateError", "An error occurred while updating your profile. Please try again.");
+            return DASHBOARD_PROFILE_SETTINGS_PAGE;
+        }
+
+        redirectAttributes.addFlashAttribute("successMessage", "Profile updated successfully!");
+        return "redirect:/user/profile-settings?success";
     }
 }
