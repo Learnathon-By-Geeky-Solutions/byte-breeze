68bf1cb3cc9763ac79b57507d8e0d0e9460a4d48
Khairul Basar <khairulbashar24853@gmail.com>
2025-02-28 12:00:27 +0600

    Rename Dashboard to PublicController and remove previous demo route

---
+++ src/main/java/com/bytebreeze/quickdrop/controller/PublicController.java
@@ -0,0 +1,20 @@
+package com.bytebreeze.quickdrop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping
+public class PublicController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Dashboard - Home");
+        return "index";  // Removed leading slash
+    }
+
+
+}
