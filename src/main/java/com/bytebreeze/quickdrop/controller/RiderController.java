16dead152d7ee3b78db39b80ec674da6e1f672ec
Khairul Basar <khairulbashar24853@gmail.com>
2025-02-28 19:46:53 +0600

    Add rider Controller for the route of riders

--- src/main/java/com/bytebreeze/quickdrop/controller/RiderController.java
+++ src/main/java/com/bytebreeze/quickdrop/controller/RiderController.java
@@ -1,4 +1,19 @@
 package com.bytebreeze.quickdrop.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/rider")
 public class RiderController {
+
+    @GetMapping("/onboarding")
+    public String showRiderOnboardingForm(){
+
+
+        return "rider/onboarding";
+    }
+
 }
