58c0a5e9668c1ebc47fde3508d6bdf540722e95e
Khairul Basar <khairulbashar24853@gmail.com>
2025-03-05 23:48:37 +0600

    Add rider registration functionality in RiderService

--- src/main/java/com/bytebreeze/quickdrop/service/RiderService.java
+++ src/main/java/com/bytebreeze/quickdrop/service/RiderService.java
@@ -1,12 +1,24 @@
 package com.bytebreeze.quickdrop.service;
 
 import com.bytebreeze.quickdrop.dto.RiderOnboardingDTO;
+import com.bytebreeze.quickdrop.dto.RiderRegistrationRequestDTO;
+import com.bytebreeze.quickdrop.dto.UserRegistrationRequestDTO;
+import com.bytebreeze.quickdrop.enums.Role;
 import com.bytebreeze.quickdrop.enums.VerificationStatus;
 //import com.bytebreeze.quickdrop.mapper.OnboardRiderMapper;
+import com.bytebreeze.quickdrop.exception.custom.AlreadyExistsException;
+import com.bytebreeze.quickdrop.exception.custom.UserNotFoundException;
+import com.bytebreeze.quickdrop.mapper.RegisterRiderMapper;
 import com.bytebreeze.quickdrop.model.Rider;
+import com.bytebreeze.quickdrop.model.User;
 import com.bytebreeze.quickdrop.repository.RiderRepository;
+import com.bytebreeze.quickdrop.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.HashSet;
 
 @RequiredArgsConstructor
 @Service
@@ -16,9 +28,41 @@ public class RiderService {
     //private final OnboardRiderMapper onboardRiderMapper;
     private final FileStorageService fileStorageService;
 
-    public Rider onboardRider(RiderOnboardingDTO riderOnboardingDTO){
+    //private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final RegisterRiderMapper registerRiderMapper;
+
+    public boolean isEmailAlreadyInUse(String email) {
+        return riderRepository.findByEmail(email).isPresent();
+
+    }
+
+    public Rider findById(Long id) {
+        return riderRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User not found"));
+    }
+
+    public Rider registerRider(RiderRegistrationRequestDTO dto){
+
+        if(isEmailAlreadyInUse(dto.getEmail())){
+
+            throw new AlreadyExistsException("Provided email already registered");
+        }
 
         Rider rider = new Rider();
+
+        dto.setPassword(passwordEncoder.encode(dto.getPassword()));
+
+        rider = registerRiderMapper.toEntity(dto);
+        if (rider.getRoles() == null || rider.getRoles().isEmpty()) {
+            rider.setRoles(new HashSet<>(Collections.singletonList(Role.ROLE_RIDER)));
+        }
+        return riderRepository.save(rider);
+    }
+
+
+    public Rider onboardRider(Long riderId, RiderOnboardingDTO riderOnboardingDTO){
+
+        Rider rider = findById(riderId);
 
         //rider = onboardRiderMapper.toEntity(riderOnboardingDTO);
         rider.setDateOfBirth(riderOnboardingDTO.getDateOfBirth());
@@ -48,7 +92,6 @@ public class RiderService {
 
         // Set verification status
         rider.setVerificationStatus(VerificationStatus.PENDING);
-
 
 
         return riderRepository.save(rider);
