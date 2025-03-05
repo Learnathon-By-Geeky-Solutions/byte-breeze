ed8b732b797eb50fd518c4bf3ddebbb973e72b75
Khairul Basar <khairulbashar24853@gmail.com>
2025-03-05 23:45:14 +0600

    Add RiderRepository

--- src/main/java/com/bytebreeze/quickdrop/repository/RiderRepository.java
+++ src/main/java/com/bytebreeze/quickdrop/repository/RiderRepository.java
@@ -1,10 +1,15 @@
 package com.bytebreeze.quickdrop.repository;
 
 import com.bytebreeze.quickdrop.model.Rider;
+import com.bytebreeze.quickdrop.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RiderRepository extends JpaRepository<Rider, Long>{
+    Optional<Rider> findByEmail(String email);
+    Optional <Rider> findById(Long id);
 
 }
