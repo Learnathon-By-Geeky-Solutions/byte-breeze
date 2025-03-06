6eabb5949e87828afef76315e0018b8e90f6a95a
Khairul Basar <khairulbashar24853@gmail.com>
2025-03-06 22:32:30 +0600

    Change Primary key type Long to UUID of User and Rider Entity

--- src/main/java/com/bytebreeze/quickdrop/repository/RiderRepository.java
+++ src/main/java/com/bytebreeze/quickdrop/repository/RiderRepository.java
@@ -6,10 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface RiderRepository extends JpaRepository<Rider, Long>{
+public interface RiderRepository extends JpaRepository<Rider, UUID>{
     Optional<Rider> findByEmail(String email);
-    Optional <Rider> findById(Long id);
+    Optional <Rider> findById(UUID id);
 
 }
