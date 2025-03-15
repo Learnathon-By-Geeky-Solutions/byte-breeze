255e5df7d09932ec47e97c35c6f72c0f8e8ce109
Khairul Basar <khairulbashar24853@gmail.com>
2025-03-16 00:57:19 +0600

    Fix issues in RiderRepository.java

--- src/main/java/com/bytebreeze/quickdrop/repository/RiderRepository.java
+++ src/main/java/com/bytebreeze/quickdrop/repository/RiderRepository.java
@@ -1,10 +1,8 @@
 package com.bytebreeze.quickdrop.repository;
 
 import com.bytebreeze.quickdrop.model.Rider;
-import com.bytebreeze.quickdrop.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 import java.util.UUID;
 
