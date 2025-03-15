f1a00b1708af6c0b8a35b01eab5b97e4acb97d29
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2025-03-15 23:12:33 +0600

    Convert RegisterRiderMapper.java into Component

--- src/main/java/com/bytebreeze/quickdrop/mapper/RegisterRiderMapper.java
+++ src/main/java/com/bytebreeze/quickdrop/mapper/RegisterRiderMapper.java
@@ -4,8 +4,10 @@ package com.bytebreeze.quickdrop.mapper;
 import com.bytebreeze.quickdrop.dto.RiderRegistrationRequestDTO;
 import com.bytebreeze.quickdrop.model.Rider;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
 @Mapper(componentModel = "spring")
+@Component
 public interface RegisterRiderMapper {
 
     Rider toEntity(RiderRegistrationRequestDTO riderRegistrationRequestDTO);
