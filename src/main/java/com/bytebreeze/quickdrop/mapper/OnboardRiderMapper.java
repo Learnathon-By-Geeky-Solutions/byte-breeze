0c28065331692b95b498e5f10386c64b70388e69
Khairul Basar <khairulbashar24853@gmail.com>
2025-03-20 18:54:07 +0600

    Fixed mapper issue for OnboardRiderMapper.java

--- src/main/java/com/bytebreeze/quickdrop/mapper/OnboardRiderMapper.java
+++ src/main/java/com/bytebreeze/quickdrop/mapper/OnboardRiderMapper.java
@@ -3,8 +3,10 @@ package com.bytebreeze.quickdrop.mapper;
 import com.bytebreeze.quickdrop.dto.RiderOnboardingDTO;
 import com.bytebreeze.quickdrop.model.Rider;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = "spring")
+@Mapper
+@Component
 public interface OnboardRiderMapper {
 
 	Rider toEntity(RiderOnboardingDTO riderOnboardingDTO);
