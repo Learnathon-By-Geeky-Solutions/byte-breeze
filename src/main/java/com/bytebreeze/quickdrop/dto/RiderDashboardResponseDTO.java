c766ab0c3a22b49d82b25eb64cd1b0edaf127bb9
Khairul Basar <khairulbashar24853@gmail.com>
2025-03-20 16:40:48 +0600

    Add Rider dashboard response DTO containing Rider name, verification status, Average rating, available status, and earning balance

---
+++ src/main/java/com/bytebreeze/quickdrop/dto/RiderDashboardResponseDTO.java
@@ -0,0 +1,23 @@
+package com.bytebreeze.quickdrop.dto;
+
+import com.bytebreeze.quickdrop.enums.VerificationStatus;
+import lombok.Data;
+
+
+@Data
+public class RiderDashboardResponseDTO {
+
+
+
+    private String fullName;
+
+    private VerificationStatus verificationStatus;
+
+    private Boolean isAvailable;
+
+    private double riderAvgRating;
+
+    private double riderBalance;
+
+
+}
