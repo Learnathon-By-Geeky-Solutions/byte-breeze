f47d543f731ad63b2afb8f7c3fc3461bf5649aa7
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2025-03-20 23:57:48 +0600

    Resolve flyway migration conflict by removing the duplication version

--- src/main/resources/db/migration/V2__changed_rider_available_column.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE riders
-    ADD is_available BOOLEAN;
-
-ALTER TABLE user_roles
-    ADD CONSTRAINT fk_user_roles_on_rider FOREIGN KEY (user_id) REFERENCES riders (id);
-
-ALTER TABLE riders
-DROP
-COLUMN want_to_delivery;
