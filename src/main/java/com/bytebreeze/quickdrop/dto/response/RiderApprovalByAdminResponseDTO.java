25bd501a6a0eb77ce1ffe8334341462afdff4c63
Khairul Basar <khairulbashar24853@gmail.com>
2025-03-20 21:27:07 +0600

    Add necessary attribute in DTO

--- src/main/java/com/bytebreeze/quickdrop/dto/response/RiderApprovalByAdminResponseDTO.java
+++ src/main/java/com/bytebreeze/quickdrop/dto/response/RiderApprovalByAdminResponseDTO.java
@@ -1,4 +1,27 @@
 package com.bytebreeze.quickdrop.dto.response;
 
+import jakarta.persistence.Column;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Data
 public class RiderApprovalByAdminResponseDTO {
+
+    private UUID id;
+
+    private String fullName;
+
+    private String email;
+
+    private String phoneNumber;
+
+    public RiderApprovalByAdminResponseDTO(UUID id, String fullName, String email, String phoneNumber) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
 }
