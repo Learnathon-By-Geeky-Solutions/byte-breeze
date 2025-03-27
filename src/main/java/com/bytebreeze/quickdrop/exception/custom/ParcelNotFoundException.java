036b9fca0cb822c165bb24bd37245246e9bea4d9
Khairul Basar <khairulbashar24853@gmail.com>
2025-03-27 23:58:28 +0600

    Add ParcelNotFoundException.java for custom exception

--- src/main/java/com/bytebreeze/quickdrop/exception/custom/ParcelNotFoundException.java
+++ src/main/java/com/bytebreeze/quickdrop/exception/custom/ParcelNotFoundException.java
@@ -1,1 +1,16 @@
 
+package com.bytebreeze.quickdrop.exception.custom;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+
+public class ParcelNotFoundException extends RuntimeException {
+
+    public ParcelNotFoundException(String message) {
+        super(message);
+    }
+
+}
