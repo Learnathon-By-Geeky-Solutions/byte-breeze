318a96be5e0611e1e49841609c5c46831fbe6998
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2025-04-15 01:13:09 +0600

    Refactor ParcelService to resolve cognitive complexity

--- src/main/java/com/bytebreeze/quickdrop/model/Payment.java
+++ src/main/java/com/bytebreeze/quickdrop/model/Payment.java
@@ -4,6 +4,8 @@ import com.bytebreeze.quickdrop.enums.PaymentStatus;
 import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.util.UUID;
+
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
