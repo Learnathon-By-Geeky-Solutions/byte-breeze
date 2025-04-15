7b8268d819430cb00abd4fd6323c495b041b8d98
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2025-04-15 21:17:10 +0600

    Refactor mapper package

---
+++ src/main/java/com/bytebreeze/quickdrop/mapper/RegisterRiderMapper.java
@@ -0,0 +1,13 @@
+package com.bytebreeze.quickdrop.mapper;
+
+import com.bytebreeze.quickdrop.dto.RiderRegistrationRequestDTO;
+import com.bytebreeze.quickdrop.model.Rider;
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+
+@Mapper(componentModel = "spring")
+@Component
+public interface RegisterRiderMapper {
+
+	Rider toEntity(RiderRegistrationRequestDTO riderRegistrationRequestDTO);
+}
